8dd5ba3dc315eb76932f693a95c9400478b5962b
Christian Fuchs <58251727+MrFox09@users.noreply.github.com>
2020-04-28 16:51:40 +0200

    update app.js
    added error handling

--- app.js
+++ app.js
@@ -13,6 +13,31 @@ const aboutRoute = require('./routes/about');
 const projectRoute = require('./routes/project');
 app.use(mainRoute,aboutRoute,projectRoute);
 
+/* Error Handling
+
+    When a non existent route is requested, creates a new Error object with a user friendly message.
+    Set the status to 404 (page not found)
+
+
+*/
+
+app.use((req, res, next) =>{
+    const err = new Error('We are sorry, page not found!');
+    err.status =404;
+    next(err); 
+  
+  
+  });
+
+  /* Takes the message and status which was set above and render a error template page, which contains a user friendly message.  */
+
+  app.use((err, req, res, next) =>{
+    res.locals.error = err;
+    res.status(err.status);
+    res.render('error');
+   
+  
+  });
 
 
 
